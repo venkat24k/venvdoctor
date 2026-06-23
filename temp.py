e75aff4bdb60a8ed52e739a79f4cccee5a74e3c3
venkat24k <venkat24092005@gmail.com>
2026-06-24 00:33:24 +0530

    Deleted temp.py as it is used for temporary testing

--- temp.py
+++
@@ -1,7 +0,0 @@
-import importlib.metadata
-
-for dist in importlib.metadata.distributions():
-    print("=" * 50)
-    print("Name:", dist.metadata.get("Name"))
-    print("Version:", dist.version)
-    print("Location:", dist.locate_file(""))
